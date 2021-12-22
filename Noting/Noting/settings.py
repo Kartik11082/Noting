827b6bc828b58096e5e1f73a5de0ca0e9146676d
Kartik <karkerakartik27@gmail.com>
2021-12-22 15:37:37 +0530

    Removed SECRET_KEY

--- Noting/Noting/settings.py
+++ Noting/Noting/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')ldzb8owk*p=jd=vlt-jjc*fob24#lvxi&ou0ox9txvu5t+_^b'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
--- Noting/auApp/views.py
+++ Noting/auApp/views.py
@@ -7,92 +7,91 @@ from random import randrange
 
 
 def usignup(request):
-    if request.method == "POST":
-        usernameUsignup = request.POST.get("signupUsername")
-        emailUsignup = request.POST.get("signupEmail")
+	if request.method == "POST":
+		usernameUsignup = request.POST.get("signupUsername")
+		emailUsignup = request.POST.get("signupEmail")
 
-        if len(usernameUsignup) < 2 or usernameUsignup is None:
-            return render(request, "usignup.html", {"msg": "Username cannot be less than 2 characters"})
+		if len(usernameUsignup) < 2 or usernameUsignup is None:
+			return render(request, "usignup.html", {"msg": "Username cannot be less than 2 characters"})
 
-        try:
-            usr = User.objects.get(username=usernameUsignup)
-            return render(request, "usignup.html", {"msg": "Username taken"})
-        except Exception as e:
-            pass
+		try:
+			usr = User.objects.get(username=usernameUsignup)
+			return render(request, "usignup.html", {"msg": "Username taken"})
+		except Exception as e:
+			pass
 
-        try:
-            # username=usernameUsignup,
-            usr = User.objects.get(email=emailUsignup)
-            return render(request, "usignup.html", {"msg": "User with this email is already registered"})
-        except User.DoesNotExist:
-            pw = ""
-            text = "abcdefghijklmnopqrstuvwxyz".upper(
-            ) + "abcdefghijklmnopqrstuvwxyz" + "0123456789"
-            for i in range(8):
-                pw += text[randrange(len(text))]
-            subject = "Welcome to Noting"
-            msg = "The password for your Noting account is " + str(pw)
-            host = "karkerakartik08@gmail.com"
-            recipient = [emailUsignup, ]
-            send_mail(subject, msg, host, recipient)
-            usr = User.objects.create_user(
-                username=usernameUsignup, password=pw, email=emailUsignup)
-            usr.save()
+		try:
+			usr = User.objects.get(email=emailUsignup)
+			return render(request, "usignup.html", {"msg": "User with this email is already registered"})
+		except User.DoesNotExist:
+			pw = ""
+			text = "abcdefghijklmnopqrstuvwxyz".upper(
+			) + "abcdefghijklmnopqrstuvwxyz" + "0123456789"
+			for i in range(8):
+				pw += text[randrange(len(text))]
+			subject = "Welcome to Noting"
+			msg = "The password for your Noting account is " + str(pw)
+			host = "karkerakartik08@gmail.com"
+			recipient = [emailUsignup, ]
+			send_mail(subject, msg, host, recipient)
+			usr = User.objects.create_user(
+				username=usernameUsignup, password=pw, email=emailUsignup)
+			usr.save()
 
-            return redirect("ulogin")
-    else:
-        return render(request, "usignup.html")
+			return redirect("ulogin")
+	else:
+		return render(request, "usignup.html")
 
 
 def ulogin(request):
-    if request.method == "POST":
-        usernameUlogin = request.POST.get("loginUsername")
-        passwordUlogin = request.POST.get("loginpassword")
-        usr = authenticate(username=usernameUlogin, password=passwordUlogin)
-        if usr is not None:
-            login(request, usr)
-            request.session["name"] = usernameUlogin
-            return redirect("home")
-        else:
-            return render(request, "ulogin.html", {"msg": "Invalid Login"})
-    else:
-        return render(request, "ulogin.html")
+	if request.method == "POST":
+		usernameUlogin = request.POST.get("loginUsername")
+		passwordUlogin = request.POST.get("loginpassword")
+		usr = authenticate(username=usernameUlogin, password=passwordUlogin)
+		if usr is not None:
+			login(request, usr)
+			request.session["name"] = usernameUlogin
+			return redirect("home")
+		else:
+			return render(request, "ulogin.html", {"msg": "Invalid Login"})
+	else:
+		return render(request, "ulogin.html")
 
 
 def uresetPass(request):
-    if request.method == "POST":
-        usernameUresetPass = request.POST.get("resetPassUsername")
-        emailUresetPass = request.POST.get("resetPassEmail")
-        try:
-            usr = User.objects.get(
-                username=usernameUresetPass, email=emailUresetPass)
-            pw = ""
-            text = "abcdefghijklmnopqrstuvwxyz".upper(
-            ) + "abcdefghijklmnopqrstuvwxyz" + "0123456789"
-            for i in range(8):
-                pw += text[randrange(len(text))]
-            subject = "Welcome to Noting"
-            msg = "The new password for your Noting account is " + str(pw)
-            host = "karkerakartik08@gmail.com"
-            recipient = [emailUresetPass, ]
-            send_mail(subject, msg, host, recipient)
-            usr.set_password(pw)
-            usr.save()
-            return redirect("ulogin")
-        except User.DoesNotExist:
-            return render(request, "uresetPass.html", {"msg": "User not registered"})
-    else:
-        return render(request, "uresetPass.html")
+	if request.method == "POST":
+		usernameUresetPass = request.POST.get("resetPassUsername")
+		emailUresetPass = request.POST.get("resetPassEmail")
+		try:
+			usr = User.objects.get(
+				username=usernameUresetPass, email=emailUresetPass)
+			pw = ""
+			text = "abcdefghijklmnopqrstuvwxyz".upper(
+			) + "abcdefghijklmnopqrstuvwxyz" + "0123456789"
+			for i in range(8):
+				pw += text[randrange(len(text))]
+			subject = "Welcome to Noting"
+			msg = "The new password for your Noting account is " + str(pw)
+			host = "karkerakartik08@gmail.com"
+			recipient = [emailUresetPass, ]
+			send_mail(subject, msg, host, recipient)
+			usr.set_password(pw)
+			usr.save()
+			return redirect("ulogin")
+		except User.DoesNotExist:
+			return render(request, "uresetPass.html", {"msg": "User not registered"})
+	else:
+		return render(request, "uresetPass.html")
 
 
 def ulogout(request):
-    logout(request)
-    request.session.flush()
-    request.session.clear_expired()
-    return redirect("ulogin")
+	logout(request)
+	request.session.flush()
+	request.session.clear_expired()
+	return redirect("ulogin")
 
 
 def deleteNote(request, noteID):
-    noteToDelete = NotingModel.objects.get(noteID=noteID)
-    noteToDelete.delete()
-    return redirect("home")
+	noteToDelete = NotingModel.objects.get(noteID=noteID)
+	noteToDelete.delete()
+	return redirect("home")
